c6419570d39780e3c34775ee30bcd41df31a9284
Scott <scott.thomas.mccleary@gmail.com>
2020-09-24 16:51:43 -0500

    Update Castoff_Reconstruction_MAIN.m

--- Castoff_Reconstruction_MAIN.m
+++ Castoff_Reconstruction_MAIN.m
@@ -70,7 +70,7 @@
 % % % 4. 'Run' (F5) the DRIVER ('Castoff_Reconstruction_DRIVER.m')  (FARO and Hemospat Drivers are provided for trialing code).
 % % % 5. 'Run' (F5) the MAIN ('Castoff_Reconstruction_MAIN.m').
 % % % 6. MAIN will output the 'Total Elapsed Cluster Analysis Time:', in seconds (s), with the total program run time excluding any variable time from user input. See User Inputs: (DRIVER) res for estimated runtimes.
-% % % 7. MAIN will save the Resultant Variables as a .mat file denoted by ˜Castoff_Reconstruction.mat and Output the Cast-off Reconstruction Results with warnings as a txt-file denoted by 'Castoff_Reconstruction_OUTPUT.txt' displayed in Figures(4+) (figure number is dependent on clustering method). Figure(1) shows the Pratt fit automated reference point. Figure(3) outputs each clustered cast-off reconstructed arc.
+% % % 7. MAIN will save the Resultant Variables as a .mat file denoted by ËœCastoff_Reconstruction.mat and Output the Cast-off Reconstruction Results with warnings as a txt-file denoted by 'Castoff_Reconstruction_OUTPUT.txt' displayed in Figures(4+) (figure number is dependent on clustering method). Figure(1) shows the Pratt fit automated reference point. Figure(3) outputs each clustered cast-off reconstructed arc.
 % % % 8. If cast-off reconstruction results suggest multiple cast-off swings, then update results by running POST with 'number_traces' reflecting the number of cast-off swings
 % % % 
 % % % Figure Displaying:
@@ -134,7 +134,7 @@ tic()
 %%%%%%%%%%%%%%%%%%%%%%%%%%  User Defined Values  %%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-data = 'Daniel_2_INPUT_DRIVER.mat';
+data = 'INK_Trial_INPUT_DRIVER.mat';
 
 load(data); %'Castoff_Reconstruction_DRIVER_SWINEBLOOD.mat' OR 'Castoff_Reconstruction_DRIVER_LISCIO_TRIAL_2_UPDATED.mat'
 sig_n = 3; %Minimum PDF Value Standard Deviations
